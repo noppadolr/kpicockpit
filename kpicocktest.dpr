fc92424929ac3c8b89fd8778e48ac8bbf27ebe70
riyasarn.n@gmail.com <noppadolr>
2019-04-14 09:01:46 +0700

    1

--- kpicocktest.dpr
+++ kpicocktest.dpr
@@ -2,7 +2,8 @@ program kpicocktest;
 
 uses
   Vcl.Forms,
-  MainForm in 'MainForm.pas' {FormMain};
+  MainForm in 'MainForm.pas' {FormMain},
+  Unit1 in 'Unit1.pas' {Form1};
 
 {$R *.res}
 
@@ -10,5 +11,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFormMain, FormMain);
+  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
